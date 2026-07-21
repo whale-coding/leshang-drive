365efe197667997bacb2b13d46b22a33653a7d60
whale-coding <njq3699@gmail.com>
2026-07-21 22:54:49 +0800

    去除无用导包

--- daijia-parent/web/web-customer/src/main/java/com/atguigu/daijia/customer/controller/OrderController.java
+++ daijia-parent/web/web-customer/src/main/java/com/atguigu/daijia/customer/controller/OrderController.java
@@ -1,9 +1,7 @@
 package com.atguigu.daijia.customer.controller;
 
-import com.atguigu.daijia.customer.service.OrderService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- daijia-parent/web/web-driver/src/main/java/com/atguigu/daijia/driver/controller/OcrController.java
+++ daijia-parent/web/web-driver/src/main/java/com/atguigu/daijia/driver/controller/OcrController.java
@@ -1,9 +1,7 @@
 package com.atguigu.daijia.driver.controller;
 
-import com.atguigu.daijia.driver.service.OcrService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
